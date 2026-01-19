9134be05dc2833277d6c4e1aeac0ff561778eac8
aadijotwani <aadijotwani@gmail.com>
2026-01-19 23:59:13 +0530

    insertion code

--- LinkedList/src/singlyLinkedList/Insertion.java
+++ LinkedList/src/singlyLinkedList/Insertion.java
@@ -1,10 +1,57 @@
 package singlyLinkedList;
 
 public class Insertion {
-
-	public static void main(String[] args) {
+	
+	class Node{
 		
-
+		int data;
+		Node next;
+		
+		public Node(int data) {
+			this.data = data;
+			next=null;
+		}		
 	}
-
+	Node head;
+	Node temp;
+	public void insert(int data) {
+		Node newNode = new Node(data);
+		
+		if(head == null) {
+			head = newNode;
+			temp = newNode;
+			return;
+		}
+		
+		temp.next = newNode;
+		temp = newNode;	
+	}
+	
+	public void printList() {
+		temp = head;
+		
+		while(temp != null) {
+			System.out.print(temp.data+" ");
+			temp = temp.next;
+		}
+		System.out.println();
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		Implementation list = new Implementation();
+		
+		list.insert(10);
+		list.insert(20);
+		list.insert(30);
+		list.insert(40);
+		
+		list.printList();
+		
+		
+		
+		
+		
+	}
 }
